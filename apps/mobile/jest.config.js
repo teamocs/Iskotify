18fe8405b28b24aab7870c7fe74bcdf48c6f9fac
chrisraro <rarochristian029@gmail.com>
2026-05-14 01:34:41 +0800

    chore(mobile): remove stale WatermelonDB references from jest config

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,26 +1,6 @@
 module.exports = {
   projects: [
     {
-      displayName: 'db',
-      testMatch: ['<rootDir>/db/**/__tests__/**/*.test.ts'],
-      testEnvironment: 'node',
-      transform: {
-        '\\.[jt]sx?$': [
-          'babel-jest',
-          {
-            caller: { name: 'metro', bundler: 'metro', platform: 'ios' },
-            configFile: './babel.config.js',
-          },
-        ],
-      },
-      transformIgnorePatterns: [
-        'node_modules/(?!(@nozbe/watermelondb))',
-      ],
-      moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/$1',
-      },
-    },
-    {
       displayName: 'services',
       testMatch: ['<rootDir>/services/**/__tests__/**/*.test.ts'],
       testEnvironment: 'node',
@@ -46,7 +26,7 @@ module.exports = {
       testMatch: ['<rootDir>/**/__tests__/**/*.test.{ts,tsx}'],
       testPathIgnorePatterns: ['<rootDir>/db/', '<rootDir>/services/'],
       transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|react-native-svg|@nozbe/watermelondb|@lineiconshq)',
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|react-native-svg|@lineiconshq)',
       ],
       setupFiles: ['./jest.setup.ts'],
       moduleNameMapper: {
